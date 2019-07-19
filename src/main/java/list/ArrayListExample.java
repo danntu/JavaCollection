4835585a320958b42f5e0c80f7b59015314593db
danik <dan_ntu@mail.ru>
2019-07-19 16:56:38 +0600

    Создать ArrayList

---
+++ src/main/java/list/ArrayListExample.java
@@ -0,0 +1,14 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+
+        List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(10,89,3,4,5,6,7,8));
+
+    }
+}
